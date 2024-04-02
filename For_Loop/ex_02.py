0bb6c005b124526e8b6711d3385aab5d88114e1b
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-02 20:45:31 +0530

    up

---
+++ For_Loop/ex_02.py
@@ -0,0 +1,20 @@
+# Example for break, continue, pass and else statement
+
+for i in range(0,10):
+    if i > 5:
+        break
+    else:
+        print(i)
+
+print()
+
+s1 = 'a b c'
+s2 = 'x y z'
+
+for i in s1:
+    for j in s2:
+        print(i, j, end = ' ')
+
+
+
+    
