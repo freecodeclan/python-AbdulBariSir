904f1f0b5666a99451675a435962322e05397012
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-03 12:14:27 +0530

    upd

---
+++ Strings/ex_01.py
@@ -0,0 +1,6 @@
+name = input("Enter your name\n")
+
+# Using for loop to iterate over the String name
+for i in name:
+    print(i, end = ' ')
+
---
+++ Strings/operators_String.py
@@ -0,0 +1,12 @@
+# Opearators on String
+
+# 1.. Concatenation 
+car1 = "Hyundai "
+car2 = "Verna"
+
+print(car1 + car2)  # By using addition + symbol we concatenate two strings
+
+# To concatenate String with integer we need to uise data type before int to convert into string
+car2 = 'verrna ' + str(1500)
+print(car2)
+
