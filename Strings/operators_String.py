991401f51972bc81306ea713b7d4aeb1e174e8b3
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-03 18:19:42 +0530

    up

--- Strings/operators_String.py
+++ Strings/operators_String.py
@@ -1,6 +1,7 @@
 # Opearators on String
 
 # 1.. Concatenation 
+
 car1 = "Hyundai "
 car2 = "Verna"
 
@@ -9,4 +10,16 @@ print(car1 + car2)  # By using addition + symbol we concatenate two strings
 # To concatenate String with integer we need to uise data type before int to convert into string
 car2 = 'verrna ' + str(1500)
 print(car2)
+
+# Indexing
+car = "Defender V110"
+for i in range(0, len(car)):
+    print(car[i], end = ' ')
+
+print()
+
+# Slicing
+name = "GODRAMA"
+sliced_name = name[1:5]
+print(sliced_name)
 
