f4729877110e3a5477c25ebb83448f46acdf6ca7
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-04 18:55:59 +0530

    up

---
+++ ASCII_Codes/ascii_representation.py
@@ -0,0 +1,8 @@
+s = 'A'
+print(ord(s))   #ord() function is used to see the ASCII value of given character
+
+s1 = 'b'
+print(ord(s1))
+
+a = 65
+print(format(a,'b'))    # Converting into binary form
---
+++ ASCII_Codes/escape_sequence.py
@@ -0,0 +1,16 @@
+print('abc\fghijk')
+
+print('\N{dollar sign}')    # '\N{} used to print uncode and symbols'
+
+print()
+
+print('\N{rupee sign}')
+
+print()
+
+a = 10
+b = 'Hello'
+c = 57.5578
+print(a,b,c,sep='-')   # sep=  is a keyword argument that you can use with the print() function to specify the separator between multiple values when they are printed.
+
+
