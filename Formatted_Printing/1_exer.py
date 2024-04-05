3aff780b667352ebe4b1dd3cec5247cff16d4db7
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-05 07:47:35 +0530

    up

---
+++ Formatted_Printing/1_exer.py
@@ -0,0 +1,12 @@
+a = 22
+b = 7
+c = a / b
+
+print('The division of {} and {} is {}'.format(a,b,c))
+
+print()
+
+name = input('Please enter your name: ')
+age = int(input('Enter your age: '))
+
+print('My name is {} and I am {} year old'.format(name,age))
