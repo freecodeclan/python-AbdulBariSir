ea60e324dbacc9ff9e665bff59c728c650544284
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-05 08:22:52 +0530

    up

--- Formatted_Printing/1_exer.py
+++ Formatted_Printing/1_exer.py
@@ -6,7 +6,19 @@ print('The division of {} and {} is {}'.format(a,b,c))
 
 print()
 
+
+# Python String .format() Method
 name = input('Please enter your name: ')
 age = int(input('Enter your age: '))
 
 print('My name is {} and I am {} year old'.format(name,age))
+
+print()
+
+# f-strings Method -- introduced in Python 3.6
+name = input('Please enter your name: ')
+age = int(input('Please enter your age: '))
+role = 'SWE'
+company = input('Please enter your company name: ')
+
+print(f'I am {name} and I am {age} years old. I work in {company} as {role}')
