246fff47c47c4dcf226c39cea22e3ca9aed81e50
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-07 22:31:02 +0530

    up

--- Tuple_Datatype/tuple_comprehension.py
+++ Tuple_Datatype/tuple_comprehension.py
@@ -9,3 +9,13 @@ print()
 
 t1 = (*(x for x in range(1,11,2)),)
 print(t1)
+
+print()
+# Now creating tuple for string
+s1 = (*(x for x in 'WELCOME'),)
+print(s1)
+
+print()
+# Now creating tuple with conditions
+s2 = (*(x for x in 'WeLcoMe' if x.islower()),)
+print(s2)
