c63e9d16373de6e47161c80ed0adbad554b56f69
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-11 20:27:36 +0530

    up

---
+++ Module/My_prgm.py
@@ -0,0 +1,11 @@
+# Here we are importing module_1 file into My_prgm file
+
+import module_1 as mo1
+
+x = 20 
+
+y = 30
+
+z = mo1.add(x,y)
+
+print(f'The sum of {x} and {y} is {z}')
---
+++ Module/module_1.py
@@ -0,0 +1,13 @@
+total_amount = 2000
+
+def add(a,b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__=='__main__':
+    print(__name__)
+    print(add(20,30))
+    print(subtract(30,10))
+
