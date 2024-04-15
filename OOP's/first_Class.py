0a6787f092d6b0fc22e9c50b612506fcea582a6a
Harsh Mehra <me.harshmehra07@gmail.com>
2024-04-15 11:59:28 +0530

    up

---
+++ OOP's/first_Class.py
@@ -0,0 +1,22 @@
+# Our first python Class
+class Cuboid:
+
+    # Constructor
+    def __init__(self,l,b,h):
+        self.length = l
+        self.breadth = b
+        self.height = h
+
+    # Method
+    def lid_Area(self):
+        return self.length * self.breadth
+    
+    def volume(self):
+        return self.length * self.breadth * self.height
+    
+# Object    
+c1 = Cuboid(10,5,3)
+print(c1.volume())
+
+c2 = Cuboid(20,10,5)
+print(c2.volume())
